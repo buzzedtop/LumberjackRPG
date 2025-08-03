64609cbde0d4f6c6736770890034d6665d88f2d7
gleatd01 <taylor@buzzedtop.com>
2025-08-03 17:43:54 -0400

    Create metal.dart

---
+++ lib/metal.dart
@@ -0,0 +1,11 @@
+class Metal {
+  final String name;
+  final double mohsHardness;
+  final String rarity;
+  final String biome;
+  final int damageBonus;
+  final int durabilityBonus;
+  final double weightModifier;
+
+  Metal(this.name, this.mohsHardness, this.rarity, this.biome, this.damageBonus, this.durabilityBonus, this.weightModifier);
+}
