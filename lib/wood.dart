298c9ec482b9f30a0cd705c8baaacb2ea33b6869
gleatd01 <taylor@buzzedtop.com>
2025-08-03 17:32:46 -0400

    Create wood.dart

---
+++ lib/wood.dart
@@ -0,0 +1,10 @@
+class Wood {
+  final String name;
+  final int jankaHardness;
+  final String rarity;
+  final String biome;
+  final int damageBonus;
+  final double weightModifier;
+
+  Wood(this.name, this.jankaHardness, this.rarity, this.biome, this.damageBonus, this.weightModifier);
+}
